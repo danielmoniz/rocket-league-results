2f410fc29b537fb08ff1cd4140200e1f5e645369
Daniel Moniz <daniel.moniz@gmail.com>
2019-03-02 13:11:22 -0500

    Clean up views file.

--- games/views.py
+++ games/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 
 from .models import Game
 
-# Create your views here.
+
 def index(request):
     return render(request, 'index.html', {})
-    # return HttpResponse("index page")
+
 
 def detail(request, id):
     game = get_object_or_404(Game, pk=id)
-    # game = Game.objects.get(pk=id)
-    # return HttpResponse(f"The game's score was: {game.get_game_result()}")
     return render(request, 'detail.html', { 'game': game })
