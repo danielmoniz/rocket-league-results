305c7587982244217ad20765ebda6157c0545247
Daniel Moniz <daniel.moniz@gmail.com>
2019-03-02 13:05:11 -0500

    Add simple templates for index and show.

--- games/views.py
+++ games/views.py
@@ -5,8 +5,10 @@ from .models import Game
 
 # Create your views here.
 def index(request):
-    return HttpResponse("index page")
+    return render(request, 'index.html', {})
+    # return HttpResponse("index page")
 
 def show(request, id):
     game = Game.objects.get(pk=id)
-    return HttpResponse(f"The game's score was: {game.get_game_result()}")
+    # return HttpResponse(f"The game's score was: {game.get_game_result()}")
+    return render(request, 'show.html', { 'game': game })
